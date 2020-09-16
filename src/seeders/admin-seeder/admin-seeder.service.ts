834b91d3cdd43999dbc807d402013c83a955162d
Artem Artemev <arteitip@gmail.com>
2020-09-16 12:59:48 +0500

    fix: remove admins seeding array

--- src/seeders/admin-seeder/admin-seeder.service.ts
+++ src/seeders/admin-seeder/admin-seeder.service.ts
@@ -4,19 +4,15 @@ import {Model} from 'mongoose';
 import {hash} from 'bcrypt';
 import {User} from '@app/schema/user.schema';
 
-import {admins} from './admins';
-
 @Injectable()
 export class AdminSeederService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   public async create() {
-    for (const admin of admins) {
-      let hashed = await hash(admin.password, 12);
-      let result = await this.userModel.create({
-        email: admin.email,
-        password: hashed,
-      });
-    }
+    let hashed = await hash(process.env.ADMIN_PASSWORD, 12);
+    let result = await this.userModel.create({
+      email: process.env.ADMIN_EMAIL,
+      password: hashed,
+    });
   }
 }
--- src/seeders/admin-seeder/admins.ts
+++
@@ -1,6 +0,0 @@
-export const admins = [
-  {
-    email: process.env.ADMIN_EMAIL,
-    password: process.env.ADMIN_PASSWORD,
-  },
-];
